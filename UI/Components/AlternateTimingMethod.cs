9bfdc913c100f062996659b894d36f179bf38da9
Dalet <dalet19@live.fr>
2014-12-19 18:36:36 +0100

    1.1.3

--- UI/Components/AlternateTimingMethod.cs
+++ UI/Components/AlternateTimingMethod.cs
@@ -136,7 +136,7 @@ namespace LiveSplit.UI.Components
         public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
         {
             var componentName = state.CurrentTimingMethod == TimingMethod.GameTime
-                ? Settings.GameTimeText : Settings.RealTimeText;
+                ? Settings.RealTimeText : Settings.GameTimeText;
             InternalComponent.LongestString = componentName;
             InternalComponent.NameLabel.Text = componentName;
             InternalComponent.TimeValue = state.CurrentTime[state.CurrentTimingMethod == TimingMethod.GameTime
